fa2f131f75c9834d1b5440f5b2bb0818b662fc05
DevmanMikey <itsmikebentley@gmail.com>
2025-09-05 04:12:02 -0700

    chore: add blueimp-md5 type declaration for TS, fix lint error

--- lib/md5.ts
+++ lib/md5.ts
@@ -2,6 +2,5 @@
 // You may want to use a more robust library in production
 // This is a placeholder for browser-side use only
 
-// @ts-expect-error
 import md5 from 'blueimp-md5';
 export default md5;
